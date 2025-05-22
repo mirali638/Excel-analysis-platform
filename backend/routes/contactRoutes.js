fcec3713f0047ada0ae99e022f00dffbd2ace059
K-Santhoshkumar <9788009123ps@gmail.com>
2025-05-22 15:18:11 +0530

    Delete backend directory

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleContact } = require("../controllers/contactController");
-
-// POST /api/contact
-router.post("/contact", handleContact);
-
-module.exports = router;
